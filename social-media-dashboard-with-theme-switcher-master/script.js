2aab7857f84796ced1ed8c5f627e42787bedaeff
Lalit Joshi <lalitjoshihld9@gmail.com>
2024-02-26 23:11:43 +0530

    Completed toggle function.

--- social-media-dashboard-with-theme-switcher-master/script.js
+++ social-media-dashboard-with-theme-switcher-master/script.js
@@ -1,5 +1,28 @@
-let theme = document.getElementsByClassName("toggle");
-let circle = document.getElementsByClassName("circle");
-theme.addEventListener('click', function() {
-    circle.classList.add("move");
+let toggleBtn = document.querySelector(".toggleBtn");
+let circle = document.querySelector(".circle");
+
+toggleBtn.addEventListener('click', function(event) {
+    event.stopPropagation();
+
+    toggleBtn.classList.toggle("active");
+    body.classList.toggle("dark");
+
+    let bodyHasDarkTheme = body.classList.contains("dark");
+
+    let themeToSave = bodyHasDarkTheme ? "dark" : "light";
+
+    localStorage.setItem("theme", themeToSave);
 })
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    let savedTheme = localStorage.getItem("theme");
+
+    body.classList.add(savedTheme);
+    let bodyHasDarkTheme = body.classList.contains("dark");
+
+    if (bodyHasDarkTheme) {
+        toggleBtn.classList.add("active");
+    }
+
+});
